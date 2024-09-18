03e2917a76d79fa49566d5819b1aaba586825637
LoickBck <loickbuck@hotmail.com>
2024-09-18 21:19:15 +0200

    homepage + connexion to db appwrite

---
+++ lib/validation.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+    name: z.string()
+    .min(2, "Le nom doit contenir 2 caractères au minimum.",)
+    .max(50, "Le nom doit contenir 50 caractères au maximum",),
+    email: z.string().email("Adresse mail incorrect."),
+    phone: z.string().refine((phone) => /^\d{10,15}$/.test(phone), 'Numéro de téléphone incorrect.')
+  })
