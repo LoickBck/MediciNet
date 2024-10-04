e77c9e2278d130406f980eb884474dfe3ed72319
LoickBck <loickbuck@hotmail.com>
2024-10-04 20:08:31 +0200

    admin page fonctionnelle

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -36,11 +36,11 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
+    <div className="data-table">
+      <Table className="shad-table">
+        <TableHeader className="bg-dark-200">
           {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
+            <TableRow key={headerGroup.id} className="shad-table-row-header">
               {headerGroup.headers.map((header) => {
                 return (
                   <TableHead key={header.id}>
@@ -62,6 +62,7 @@ export function DataTable<TData, TValue>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                className="shad-table-row"
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
@@ -79,18 +80,20 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="table-actions">
         <Button
           variant="outline"
           size="sm"
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
+          className="shad-gray-btn"
         >
           <Image
-            src="/assets/icons/left.svg"
+            src="/assets/icons/arrow.svg"
             width={24}
             height={24}
             alt="précédent"
+            className="rotate-180"
           />
         </Button>
         <Button
@@ -98,9 +101,10 @@ export function DataTable<TData, TValue>({
           size="sm"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
+          className="shad-gray-btn"
         >
           <Image
-            src="/assets/icons/right.svg"
+            src="/assets/icons/arrow.svg"
             width={24}
             height={24}
             alt="suivant"
