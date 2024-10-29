98da3a1b6c7a4e92d0ebbe2de2223331845b7d08
LoickBck <loickbuck@hotmail.com>
2024-10-29 15:30:39 +0100

    fix bug deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
+import "dotenv/config";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "build",
   images: {
     domains: ["cloud.appwrite.io"], // Ajoutez le domaine ici
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
   },
 };
 
