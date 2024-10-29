af456320ec7c028e1a7d321b92e7fb350685b04f
LoickBck <loickbuck@hotmail.com>
2024-10-29 03:16:51 +0100

    protection route admin + page erreur

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,37 +6,71 @@ import StatCard from "@/components/StatCard";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import { DataTable } from "@/components/DataTable";
 import { columns } from "@/components/table/columns";
+import { useRouter } from "next/navigation";
+import { decryptKey } from "@/lib/utils"; // Importation de la fonction de décryptage pour vérifier l'accès.
 
 const Admin = () => {
-  // Ajout de l'état pour stocker les rendez-vous et les informations de chargement
+  const router = useRouter();
+
+  // États pour stocker les données des rendez-vous, l'état de chargement et l'autorisation d'accès
   const [appointments, setAppointments] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isAccessGranted, setIsAccessGranted] = useState(false);
 
+  // Vérifie si l'utilisateur a le droit d'accéder à la page admin
   useEffect(() => {
-    // Fonction asynchrone pour récupérer les données et les définir dans l'état
-    const fetchAppointments = async () => {
-      try {
-        const data = await getRecentAppointmentList();
-        setAppointments(data);
-      } catch (error) {
-        console.error("Erreur lors du chargement des rendez-vous :", error);
-      } finally {
-        setLoading(false); // Désactive l'état de chargement une fois terminé
+    const checkAccess = () => {
+      const encryptedKey = window.localStorage.getItem("accessPass"); // Récupère l'accessPass crypté dans le stockage local
+      if (encryptedKey) {
+        const accessPass = decryptKey(encryptedKey); // Décrypte l'accessPass
+        if (accessPass === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
+          setIsAccessGranted(true); // Si le mot de passe est correct, accorde l'accès
+        } else {
+          router.push("/rendez-vous?admin=true"); // Redirige vers la page de connexion si le mot de passe est incorrect
+        }
+      } else {
+        router.push("/rendez-vous?admin=true"); // Redirige si aucun accessPass n'est présent
       }
     };
 
-    fetchAppointments(); // Exécution de la fonction de récupération de données
-  }, []);
+    checkAccess();
+  }, [router]);
 
+  // Charge les données des rendez-vous si l'accès est accordé
+  useEffect(() => {
+    if (isAccessGranted) {
+      const fetchAppointments = async () => {
+        try {
+          const data = await getRecentAppointmentList(); // Récupère la liste des rendez-vous
+          setAppointments(data);
+        } catch (error) {
+          console.error("Erreur lors du chargement des rendez-vous :", error);
+        } finally {
+          setLoading(false); // Désactive l'état de chargement une fois terminé
+        }
+      };
+
+      fetchAppointments(); // Exécution de la fonction de récupération de données
+    }
+  }, [isAccessGranted]);
+
+  // N'affiche rien si l'accès n'est pas validé
+  if (!isAccessGranted) {
+    return null;
+  }
+
+  // Affiche un spinner pendant le chargement des données
   if (loading)
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-green-500"></div>
       </div>
-    ); // Message pendant le chargement des données
+    );
 
+  // Affichage du contenu principal de la page d'administration
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      {/* En-tête de la page avec le logo et le titre */}
       <header className="admin-header">
         <Link href="/" className="cursor-pointer">
           <Image
@@ -51,6 +85,7 @@ const Admin = () => {
       </header>
 
       <main className="admin-main">
+        {/* Section d'accueil avec un message de bienvenue */}
         <section className="w-full space-y-4">
           <h1 className="header">Bienvenue 👋</h1>
           <p className="text-dark-700">
@@ -86,6 +121,7 @@ const Admin = () => {
           </div>
         </section>
 
+        {/* Section de statistiques sur les rendez-vous */}
         <section className="admin-stat">
           <StatCard
             type="programmer"
@@ -99,7 +135,6 @@ const Admin = () => {
             label="Rendez-vous en attente"
             icon="/assets/icons/pending.svg"
           />
-
           <StatCard
             type="annuler"
             count={appointments?.cancelledCount || 0}
@@ -108,6 +143,7 @@ const Admin = () => {
           />
         </section>
 
+        {/* Table de données pour afficher la liste des rendez-vous */}
         {appointments && (
           <DataTable columns={columns} data={appointments.documents} />
         )}
--- components/PasswordModal.tsx
+++ components/PasswordModal.tsx
@@ -21,27 +21,42 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { decryptKey, encryptKey } from "@/lib/utils";
 
+const EXPIRATION_TIME = 60 * 60 * 1000; // 1 heure en millisecondes
+
 const PasswordModal = () => {
   const router = useRouter();
   const [open, setOpen] = useState(true);
   const path = usePathname();
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+
   const encryptedKey =
     typeof window !== "undefined"
       ? window.localStorage.getItem("accessPass")
       : null;
+  const expirationTime =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessPassExpiration")
+      : null;
+
   useEffect(() => {
     const accessPass = encryptedKey && decryptKey(encryptedKey);
-    if (path) {
-      if (accessPass === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
-        setOpen(false);
-        router.push("/admin");
-      } else {
-        setOpen(true);
-      }
+    const now = Date.now();
+
+    if (
+      accessPass === process.env.NEXT_PUBLIC_ADMIN_PASSWORD &&
+      expirationTime &&
+      now < Number(expirationTime)
+    ) {
+      setOpen(false);
+      router.push("/admin");
+    } else {
+      // Si expiré ou non valide, ouvrir la modal et nettoyer localStorage
+      localStorage.removeItem("accessPass");
+      localStorage.removeItem("accessPassExpiration");
+      setOpen(true);
     }
-  }, [encryptedKey]);
+  }, [encryptedKey, expirationTime]);
 
   const validatePassword = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -50,13 +65,19 @@ const PasswordModal = () => {
 
     if (password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
       const encryptedKey = encryptKey(password);
+      const expirationDate = Date.now() + EXPIRATION_TIME;
 
+      // Sauvegarde de la clé encryptée et de l'expiration
       localStorage.setItem("accessPass", encryptedKey);
+      localStorage.setItem("accessPassExpiration", expirationDate.toString());
+
       setOpen(false);
+      router.push("/admin");
     } else {
       setError("Mot de passe invalide. Veuillez réessayer.");
     }
   };
+
   const closeModal = () => {
     setOpen(false);
     router.push("/");
